f4978406cbd82c6d6360aba7a5364e6cca8946a6
Turk <ko.turk@rabobank.nl>
2023-05-15 18:25:11 +0200

    TEQNation: added Apache Pulsar / Kafka projects.

---
+++ TEQNation/Pulsar/basic/src/main/java/nl/datastax/pulsar/PaymentEngineApplication.java
@@ -0,0 +1,11 @@
+package nl.datastax.pulsar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaymentEngineApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentEngineApplication.class, args);
+    }
+}
